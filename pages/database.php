0b45b571c47e6d5dab776517213e315e8e9820ac
Audiovisuaali <aleksi.vuorio@yahoo.com>
2018-01-25 19:11:00 +0200

    Update database.php

--- pages/database.php
+++ pages/database.php
@@ -2,13 +2,13 @@
 
 // Mysql credentials
 $servername = "localhost";
-$username = "audiovisuaali_url_user";
-$password = "pb56RB";
+$username = "";
+$password = "";
 $dbname = "";
 
 // Creating connection
 try {
-    $handler = new PDO("mysql:host=$servername;dbname=audiovisuaali_url", $username, $password);
+    $handler = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
